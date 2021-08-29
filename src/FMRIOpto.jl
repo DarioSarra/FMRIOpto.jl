8b7101b7d2864f5da00bc792c6bfb06a1d9f5f93
DarioSarra <dariosarra@gmail.com>
2021-08-29 16:18:10 +0100

    added browse table

--- src/FMRIOpto.jl
+++ src/FMRIOpto.jl
@@ -1,8 +1,8 @@
 module FMRIOpto
 
-using Reexport
-@reexport using DataFrames, CSV, CategoricalArrays
-@reexport using Statistics, StatsBase, Plots, StatsPlots, HypothesisTests
-@reexport using MixedModels
+    using Reexport
+    @reexport using DataFrames, CSV, CategoricalArrays, BrowseTable
+    @reexport using Statistics, StatsBase, Plots, StatsPlots, HypothesisTests
+    @reexport using MixedModels
 
 end # module
